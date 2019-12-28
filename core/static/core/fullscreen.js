1b9769b96bd7286ac9f6c540215ff10bd3cffa36
gnikwo <gnikwo@hotmail.com>
2019-12-28 10:59:45 +0100

    Removed untracked files

---
+++ core/static/core/fullscreen.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function(){
+    var cameras = document.getElementsByClassName("camera__video");
+
+    for(var i = 0; i < cameras.length; i++) {
+        cameras.item(i).addEventListener('click', function(e) {
+            toggleFullscreen(e.target);
+        });
+    }
+});
+
+function toggleFullscreen(elem) {
+    elem = elem || document.documentElement;
+    if (!document.fullscreenElement && !document.mozFullScreenElement &&
+        !document.webkitFullscreenElement && !document.msFullscreenElement) {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        } else if (elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) {
+            elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+}
+
